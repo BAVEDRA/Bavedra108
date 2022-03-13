9d977008dd8b531459f43fbe7a18ece96cd3ad99
BAVEDRA <90138201+BAVEDRA@users.noreply.github.com>
2022-03-13 10:39:11 +0100

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,49 @@
+$('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+
+
+$("#reg_form").on("submit",function(){
+   var values = $(this).serializeArray();
+   
+   var udata = {"name":values[0]["value"],"password":values[1]["value"],"email":values[2]["value"],}
+  
+   $.ajax({
+      url:"https://aicl-web.herokuapp.com/create_user",
+      type:"POST",
+      data:JSON.stringify(udata),
+      dataType:"JSON",
+      contentType: "application/json; charset=utf-8",
+      success:function(data){
+         alert("user created!")
+         console.log(data)
+        
+      }
+      
+
+      
+   })
+
+
+})
+
+$("#login_form").on("submit",function(){
+   var values = $(this).serializeArray();
+   var udata = {"name":values[0]["value"],"password":values[1]["value"]}
+   $.ajax({
+      url:"https://aicl-web.herokuapp.com/login_user",
+      type:"POST",
+      data:JSON.stringify(udata),
+      dataType:"JSON",
+      contentType: "application/json; charset=utf-8",
+      success:function(data){
+         alert(data["status"])
+         
+        
+      }
+      
+
+      
+   })
+})
